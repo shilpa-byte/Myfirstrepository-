7236442246b857179149a5b8cbc4f171ac2f3abe
AShilpa <AShilpa@BLRL-5CG9191T8K.scientificgames.com>
2020-07-03 08:39:37 +0530

    My first commit

---
+++ src/test/java/com/actitime/login/demo.java
@@ -0,0 +1,14 @@
+package com.actitime.login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	
+@Test	
+public void run()
+{
+	Reporter.log("I am Shilpa",true);
+}
+}
